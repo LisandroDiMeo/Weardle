f3bbd94a8f8a37e959277fab42ab7b495c083038
Lisandro.Diaz <lisandro.diaz@naranja.com>
2022-04-26 08:46:32 -0300

    yellow coloring of letters now works fine

--- app/src/main/java/com/example/android/wearable/wearwordle/gamelogic/WordGuess.kt
+++ app/src/main/java/com/example/android/wearable/wearwordle/gamelogic/WordGuess.kt
@@ -13,6 +13,17 @@ class WordGuess(private val word: String, private val onGuessCorrect : () -> Uni
 
     private var attempts = 0
 
+    private val charCounts = mutableMapOf<Char, Int>()
+
+    init {
+        word.forEach { char ->
+            if(charCounts.containsKey(char))
+                charCounts[char] = charCounts[char]!! + 1
+            else
+                charCounts[char] = 1
+        }
+    }
+
     private fun toAnnotatedString(guessState : List<Pair<Char, Guess>>) : AnnotatedString {
         return guessState.foldRight(AnnotatedString("")) { pair, acc ->
             annotatedStringOf(pair) + acc
@@ -24,20 +35,45 @@ class WordGuess(private val word: String, private val onGuessCorrect : () -> Uni
             Guess.GREEN -> { AnnotatedString("${p.first}", GREEN_SPAN) }
             Guess.YELLOW -> { AnnotatedString("${p.first}", YELLOW_SPAN) }
             Guess.GRAY -> { AnnotatedString("${p.first}", GRAY_SPAN) }
+            else -> AnnotatedString("${p.first}", GRAY_SPAN)
         }
     }
 
     fun guessWord(guess: String) : AnnotatedString {
         attempts ++
+        val auxCharCounts = mutableMapOf<Char, Int>()
+        auxCharCounts.putAll(charCounts)
         val guessResult = guess.mapIndexed { index, char ->
             when (char) {
                 word[index] -> char to Guess.GREEN
-                in word -> char to Guess.YELLOW
-                else -> char to Guess.GRAY
+                !in word -> char to Guess.GRAY
+                else -> char to Guess.MAYBE_YELLOW
             }
         }
+        val guessResultWithCorrection = guessResult.map { pair ->
+            if(pair.second == Guess.MAYBE_YELLOW){
+                val countOfThisChar = auxCharCounts[pair.first]!!
+                if(countOfThisChar == 1 && !guessResult.any { it.second == Guess.GREEN || it.second == Guess.YELLOW }){
+                    auxCharCounts[pair.first] = 0
+                    pair.first to Guess.YELLOW
+                }else{
+                    var greens = 0
+                    guessResult.filter {it.first == pair.first}.forEach { if(it.second == Guess.GREEN) greens++ }
+                    if(greens < countOfThisChar){
+                        auxCharCounts[pair.first] = auxCharCounts[pair.first]!! - 1
+                        pair.first to Guess.YELLOW
+                    }else{
+                        pair.first to Guess.GRAY
+                    }
+                }
+            }
+            else {
+                pair
+            }
+        }
+
         guessResult.filter { it.second != Guess.GREEN }.let { if ( it.isEmpty() ) onGuessCorrect.invoke() else if ( attempts == 5 ) onGameOver.invoke() }
-        return toAnnotatedString(guessResult)
+        return toAnnotatedString(guessResultWithCorrection)
     }
 
     companion object {
@@ -49,5 +85,5 @@ class WordGuess(private val word: String, private val onGuessCorrect : () -> Uni
 }
 
 enum class Guess {
-    GREEN, YELLOW, GRAY
+    GREEN, YELLOW, GRAY, MAYBE_YELLOW
 }
